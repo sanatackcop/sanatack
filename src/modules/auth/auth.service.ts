d88a3b9a60647fa5ca0b945be734f1d3eb758472
Abdulmajeed Ali Al-Hazemi <abdulmajeedalhazemi@gmail.com>
2025-03-03 21:54:44 +0300

    Update auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async validateOAuthLogin(profile: any): Promise<any> {
+    // Implement your logic to validate and handle the OAuth login
+    return profile;
+  }
+}
