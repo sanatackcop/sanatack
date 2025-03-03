5642c19f69b914ae64a4d0da7eab9ba770a4f9d8
Abdulmajeed Ali Al-Hazemi <abdulmajeedalhazemi@gmail.com>
2025-03-03 21:54:49 +0300

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { GithubStrategy } from './strategies/github.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
+  imports: [PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, GithubStrategy, GoogleStrategy],
 })
 export class AuthModule {}
