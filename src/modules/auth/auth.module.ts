d82c612cd164f3042e2ddf80536b5e59d7f11966
Abdulmajeed Ali Al-Hazemi <abdulmajeedalhazemi@gmail.com>
2025-03-03 22:05:47 +0300

    Update auth.module.ts

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GithubStrategy } from './strategies/github.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, GithubStrategy, GoogleStrategy],
+  providers: [
+    AuthService,
+    GithubStrategy,
+    GoogleStrategy,
+    JwtAuthGuard,
+    RolesGuard,
+  ],
 })
 export class AuthModule {}
