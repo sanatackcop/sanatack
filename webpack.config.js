cbe080bafb32380c83d0bd80ff35ee17ccdd2839
sanatack-org <srosamazaid@gmail.com>
2025-03-16 03:56:07 +0300

    ci/cd

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/main.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+};
