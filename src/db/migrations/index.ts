4fea08f7b94eb1ab02857348402c8d9246e43040
Mariam Aslan <140263162+mariamM13@users.noreply.github.com>
2025-04-28 20:12:32 +0300

    add migrations

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,9 +1,11 @@
 import { AddUserTokenTable1741999709886 } from './1741999709886-AddUserTokenTable';
 import { AddedCoursesLogic1744993675068 } from './1744993675068-AddedCoursesLogic';
 import { AddTableCourseProgress1745510805070 } from './1745510805070-AddTableCourseProgress';
+import { AddEnrollmentTable1745779688646 } from './1745779688646-AddEnrollmentTable';
 
 export const migrationFiles = [
   AddUserTokenTable1741999709886,
   AddedCoursesLogic1744993675068,
-  AddTableCourseProgress1745510805070
+  AddTableCourseProgress1745510805070,
+  AddEnrollmentTable1745779688646
 ];
