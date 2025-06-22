b527e92d4317d70bfafb4508b9825a1322e6e391
Mariam Aslan <140263162+mariamM13@users.noreply.github.com>
2025-06-22 14:43:55 +0300

    merge

--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -5,7 +5,8 @@ import { AddedArticleTable1750524459147 } from './1750524459147-AddedArticleTabl
 import { UserAttributesTable1750264341355 } from './1750264341355-UserAttributesTable';
 
 export const migrationFiles = [
-  FullMig1749751469450,UserAttributesTable1750264341355,
+  FullMig1749751469450,
+  UserAttributesTable1750264341355,
   UpdateCourseCreation1750185723559,
   UpdateCourseEntity1750500683013,
   AddedArticleTable1750524459147,
